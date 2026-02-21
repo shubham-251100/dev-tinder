2790d60150678e7ed7f05a6cd1c5decd20bd6ccb
Shubham Rawat <shubham25112000@gmail.com>
2026-02-21 17:20:35 +0530

    Routes, path params, search query

--- src/app.js
+++ src/app.js
@@ -4,8 +4,52 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use("/login", (req, res) => {
+app.get("/user", (req, res) => {
+    res.send({
+        name: "Shubham Rawat",
+        class: "12th"
+    });
+});
+
+app.post("/user", (req, res) => {
+    res.send({
+        name: "Aman",
+        class: "8th"
+    })
+});
+
+app.put("/user/:id", (req, res) => {
+    const qurey = req.query;
+    const path = req.path;
+    const params = req.params;
+    res.send({
+        name: "Put Call",
+        qurey,
+        path,
+        params,
+    })
+
+    // PUT: http://localhost:3000/user/1?name=What
+
+    // {
+    //     "name": "Put Call",
+    //     "qurey": {
+    //         "name": "What"
+    //     },
+    //     "path": "/user/1",
+    //     "params": {
+    //         "id": "1"
+    //     }
+    // }
+})
+
+// We can use different pattens in the /path and even regex
+app.use("/login*system", (req, res) => {
     res.send("I am login page");
+
+    // GET: http://localhost:3000/login-to-system
+
+    // I am login page
 })
 
 app.use("/signup", (req, res) => {
